37c6720d6d066003d6153c5374ba63a8731209b2
Stephanie Barker <song_mage@yahoo.com>
2017-08-02 13:05:42 -0400

    working on joins

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -160,3 +160,43 @@ SELECT id,stadium,team1,team2
 SELECT player, teamid, stadium, mdate
  FROM game JOIN goal ON (id=matchid)
  WHERE teamid = 'GER';
+
+SELECT team1, team2, player
+ FROM game JOIN goal ON (id=matchid)
+ WHERE player LIKE 'Mario%'
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid=id
+ WHERE gtime<=10
+
+SELECT mdate, teamname
+FROM game JOIN eteam ON eteam.id = team1
+WHERE coach = 'Fernando Santos'
+
+SELECT player
+FROM goal JOIN game ON matchid = id
+WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE (team1='GER' OR team2='GER')
+    AND (teamid != 'GER')
+
+SELECT teamname, COUNT(*)
+  FROM eteam JOIN goal ON id=teamid
+ GROUP BY teamname
+
+SELECT stadium, COUNT(*)
+FROM game JOIN goal ON id = matchid
+GROUP BY stadium
+
+SELECT matchid, mdate, COUNT(*)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid, mdate
+
+SELECT matchid, mdate, COUNT(*)
+FROM game JOIN goal ON id = matchid
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate
+
